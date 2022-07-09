7530e7813d71925050a4e2e3a59057a08cd93c36
Sezer Bozbıyık <sezer.bozbiyik@icloud.com>
2022-07-09 22:50:41 +0300

    updated fivedayweather

--- src/forecast.js
+++ src/forecast.js
@@ -1,6 +1,6 @@
 class Forecast {
   constructor() {
-    this.apikey = "xYMoaMnL9XXXJkGN16ds8DLsH4YyHMN2";
+    this.apikey = "UkGBeo0pMytGsUBGhspg0hDhhWCIjxGT";
     this.cityApi =
       "https://dataservice.accuweather.com/locations/v1/cities/search";
     this.weatherApi =
@@ -9,6 +9,18 @@ class Forecast {
       "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
     this.oneDayWeatherApi =
       "https://dataservice.accuweather.com/forecasts/v1/daily/1day/";
+    this.fiveDayWeatherApi =
+      "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
+  }
+
+  async getFiveDayWeather(cityKey) {
+    const response = await fetch(
+      this.fiveDayWeatherApi +
+        cityKey +
+        `?apikey=${this.apikey}&details=true&metric=true`
+    );
+    const data = await response.json(response);
+    return data.DailyForecasts;
   }
 
   async getGeoCity(lat, lon) {
@@ -31,7 +43,7 @@ class Forecast {
     const response = await fetch(
       this.weatherApi + cityKey + `?apikey=${this.apikey}&details=true`
     );
-    const data = await response.json();
+    const data = await response.json(response);
     return data[0];
   }
   async getOneDayWeather(cityKey) {
@@ -40,7 +52,7 @@ class Forecast {
         cityKey +
         `?apikey=${this.apikey}&details=true&metric=true`
     );
-    const data = await response.json();
+    const data = await response.json(response);
     return data.DailyForecasts[0];
   }
 
@@ -48,14 +60,16 @@ class Forecast {
     const cityDto = await this.getCity(city);
     const oneDayWeather = await this.getOneDayWeather(cityDto.Key);
     const weatherDto = await this.getWeather(cityDto.Key);
-    return { cityDto, oneDayWeather, weatherDto };
+    const fiveDayWeather=await this.getFiveDayWeather(cityDto.Key);
+    return { cityDto, oneDayWeather, weatherDto ,fiveDayWeather};
   }
 
   async updateCityByLocation(lat, lon) {
     const cityDto = await this.getGeoCity(lat, lon);
     const oneDayWeather = await this.getOneDayWeather(cityDto.Key);
     const weatherDto = await this.getWeather(cityDto.Key);
-    return { cityDto, oneDayWeather, weatherDto };
+    const fiveDayWeather = await this.getFiveDayWeather(cityDto.Key);
+    return { cityDto, oneDayWeather, weatherDto, fiveDayWeather};
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Forecast from "./forecast";
 import format from "date-fns/format";
+import { intervalToDuration } from "date-fns";
 const forecast = new Forecast();
 
 // dom elements
@@ -17,26 +18,48 @@ const humidity = document.querySelector(".humidity>.card-md-body");
 const visibility = document.querySelector(".visibility>.card-md-body");
 const isDayImg = document.querySelector(".location img");
 const searchBox = document.querySelector("form#search");
+const sunrise = document.querySelector(".sunrise-text>span:first-child");
+const sunriseInterval = document.querySelector(".sunrise-text>span:last-child");
+const sunset = document.querySelector(".sunset-text>span:first-child");
+const sunsetInterval = document.querySelector(".sunset-text>span:last-child");
+const weekCards = document.querySelector(".week-cards");
 
 // change UI by
 const updateUI = (data) => {
-  const { cityDto, oneDayWeather, weatherDto } = data;
-
-  const now = new Date();
+  // create manipule variables
+  const { cityDto, oneDayWeather, weatherDto,fiveDayWeather } = data;
+  const now = new Date(weatherDto.LocalObservationDateTime.slice(0, 19));
   const day = format(now, "EEEE");
   const time = format(now, "HH") + ":" + format(now, "mm");
+  const sunsetTime = new Date(oneDayWeather.Sun.Set.slice(0, 19));
+  const sunriseTime = new Date(oneDayWeather.Sun.Rise.slice(0, 19));
 
   //dom manipulation
+  
+  createFiveDayHtml(fiveDayWeather);
+  
+  sunrise.textContent = format(sunriseTime, "p");
+  sunriseInterval.textContent = getSunriseTime(sunriseTime, now);
+
+  sunset.textContent = format(sunsetTime, "p");
+  sunsetInterval.textContent = getSunsetTime(sunsetTime, now);
+
   dateTime.innerHTML = `${day}, <span>${time}</span>`;
+
   temp.innerHTML = `${Math.round(
     weatherDto.Temperature.Metric.Value
   )}<sup>&deg;C</sup>`;
+
   weatherCon.innerHTML = `<img src="img/weather/${weatherDto.WeatherIcon}.png" height="20" alt="">
   ${weatherDto.WeatherText}`;
+
   uvText.textContent = weatherDto.UVIndex;
+
   windRate.innerHTML = `${weatherDto.Wind.Speed.Metric.Value} <span>km/h</span>`;
   windDirection.textContent = weatherDto.Wind.Direction.English;
+
   rainRatio.textContent = oneDayWeather.Day.RainProbability + "%";
+
   cityName.textContent = `${
     cityDto.SupplementalAdminAreas.length > 0
       ? cityDto.SupplementalAdminAreas[0].EnglishName
@@ -44,8 +67,11 @@ const updateUI = (data) => {
   }(${cityDto.EnglishName}), ${cityDto.AdministrativeArea.EnglishName}, ${
     cityDto.Country.EnglishName
   }`;
+
   humidity.innerHTML = `${weatherDto.RelativeHumidity} <sup>&percnt;</sup>`;
+
   visibility.innerHTML = `${weatherDto.Visibility.Metric.Value} <span>km</span>`;
+
   weatherIcon.setAttribute(
     "src",
     "img/weather/" + weatherDto.WeatherIcon + ".png"
@@ -60,20 +86,18 @@ const updateUI = (data) => {
     cityName.classList.remove("img-text-dark");
     cityName.classList.add("img-text-light");
   }
-
-  console.log(data, now.toTimeString());
 };
 
 // get current location weather
 locationBtn.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition((d) => {
     const lat = d.coords.latitude;
-    const lon = d.coords.longitude;
+    const lng = d.coords.longitude;
     localStorage.setItem("lat", lat);
-    localStorage.setItem("lon", lon);
+    localStorage.setItem("lng", lng);
     localStorage.removeItem("city");
     forecast
-      .updateCityByLocation(lat, lon)
+      .updateCityByLocation(lat, lng)
       .then((data) => updateUI(data))
       .catch((err) => console.log(err));
   });
@@ -82,22 +106,23 @@ locationBtn.addEventListener("click", () => {
 // get weather by search
 searchBox.addEventListener("submit", (e) => {
   e.preventDefault();
-  localStorage.removeItem('lat');
-  localStorage.setItem('city',e.target.search.value)
+  const cityName = e.target.search.value.trim();
+  localStorage.removeItem("lat");
+  localStorage.removeItem("lng");
+  localStorage.setItem("city", cityName);
   forecast
-    .updateCityByName(e.target.search.value)
+    .updateCityByName(cityName)
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
-  searchBox.reset();
-  location.reload();
+  searchBox.submit();
 });
 
 // // local storage update UI
-if (localStorage.getItem("lat") && localStorage.getItem("lon")) {
+if (localStorage.getItem("lat") && localStorage.getItem("lng")) {
   forecast
     .updateCityByLocation(
       localStorage.getItem("lat"),
-      localStorage.getItem("lon")
+      localStorage.getItem("lng")
     )
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
@@ -107,3 +132,51 @@ if (localStorage.getItem("lat") && localStorage.getItem("lon")) {
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 }
+
+// another function clean code refactor
+const getSunsetTime = (sunset, now) => {
+  let info = "";
+  now > sunset ? (info = "+") : (info = "-");
+  const sunsetTime = new Date(sunset);
+  const intervalSunset = intervalToDuration({ start: now, end: sunsetTime });
+
+  return intervalSunset.hours > 0
+    ? info + intervalSunset.hours + "h " + intervalSunset.minutes + "m"
+    : info + intervalSunset.minutes + "m";
+};
+
+const getSunriseTime = (sunrise, now) => {
+  let info = "";
+  now > sunrise ? (info = "+") : (info = "-");
+  const sunriseTime = new Date(sunrise);
+  const intervalSunrise = intervalToDuration({ start: now, end: sunriseTime });
+
+  return intervalSunrise.hours > 0
+    ? info + intervalSunrise.hours + "h " + intervalSunrise.minutes + "m"
+    : info + intervalSunrise.minutes + "m";
+};
+
+// forecast
+//   .getFiveDayWeather(3558515)
+//   .then((d) => {
+//     console.log(d);
+//     createFiveDayHtml(d);
+//   })
+//   .catch((err) => console.log(err));
+
+// create 5 day html template
+const createFiveDayHtml = (data) => {
+  let html = "";
+  data.forEach((d) => {
+    html += `<div class="card-sm">
+    <span class="card-sm-title">${format(new Date(d.Date), "EE")}</span>
+    <img src="img/weather/${d.Day.Icon}.png" height="35" alt="">
+    <span class="card-sm-footer">${Math.round(
+      d.Temperature.Maximum.Value
+    )}&deg; <span class="text-muted">${Math.round(
+      d.Temperature.Minimum.Value
+    )}&deg;</span> </span>
+</div>`;
+  });
+  weekCards.innerHTML = html;
+};
